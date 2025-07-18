e63476f483ce19cf9167004ee1c790cf98b13ed0
yusuke0402 <nakyu0402@gmail.com>
2025-07-18 16:09:57 +0900

    psの測定を修正、層の中に何もないときにエラーが起こる

--- main.py
+++ main.py
@@ -19,20 +19,20 @@ for j in range(0,config["hyperparam"]["n_trial"]):
     data=DataSets()   
 
     #2.傾向スコアの推定
-    ppscore_target_1,ppscore_source1 = propensityscore(target_x=data.training_current_x[:,1:],target_y=data.training_current_y,source_x=data.training_historical_1_x[:,1:],source_y=data.training_historical_1_y)
+    ppscore_target,ppscore_source = propensityscore(target_x=data.training_current_x[:,1:],target_y=data.training_current_y,source_x=data.training_historical_x[:,1:],source_y=data.training_historical_y)
 
     #3.傾向スコアに基づいてデータを層別化・トリミング
-    target_1_data,split_1_values=target_trim(data.training_current_x,data.training_current_y.reshape(-1),ppscore_target_1)
-    source_1_data=source_trim(data.training_historical_1_x,data.training_historical_1_y.reshape(-1),ppscore_source1,split_1_values)
+    target_data,split_values=target_trim(data.training_current_x,data.training_current_y.reshape(-1),ppscore_target)
+    source_data=source_trim(data.training_historical_x,data.training_historical_y.reshape(-1),ppscore_source,split_values)
 
     #4.層ごとに平均治療効果を推定
     for i in range(0,split):
-        target_1_condition = target_1_data['Group'] == i+1
-        source_1_condition = source_1_data['Group'] == i+1  
-        target_1_group=target_1_data[target_1_condition]
-        source_1_group=source_1_data[source_1_condition]
-        average_treatment_effect = np.mean(target_1_group['Outcomes'].to_numpy()) - np.mean(source_1_group['Outcomes'].to_numpy())
-        weight=(len(source_1_group['Outcomes'])+len(target_1_group['Outcomes']) )/ (len(source_1_data['Outcomes'])+len(target_1_data['Outcomes']))
+        target_condition = target_data['Group'] == i+1
+        source_condition = source_data['Group'] == i+1  
+        target_group=target_data[target_condition]
+        source_group=source_data[source_condition]
+        average_treatment_effect = np.mean(target_group['Outcomes'].to_numpy()) - np.mean(source_group['Outcomes'].to_numpy())
+        weight=(len(source_group['Outcomes'])+len(target_group['Outcomes']) )/ (len(source_data['Outcomes'])+len(target_data['Outcomes']))
         est_theta[j] += average_treatment_effect* weight
     
 print("thetaの平均値：",np.mean(est_theta))
